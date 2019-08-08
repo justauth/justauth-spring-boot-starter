03977a9567b0428db132300089c8165f586e05f2
Yangkai.Shen <237497819@qq.com>
2019-08-08 16:22:36 +0800

    :memo: 添加 LICENSE 和 COPYRIGHT

--- src/main/java/com/xkcoding/justauth/properties/JustAuthProperties.java
+++ src/main/java/com/xkcoding/justauth/properties/JustAuthProperties.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019-2029, xkcoding & Yangkai.Shen & 沈扬凯 (237497819@qq.com & xkcoding.com).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.xkcoding.justauth.properties;
 
 import lombok.Getter;
