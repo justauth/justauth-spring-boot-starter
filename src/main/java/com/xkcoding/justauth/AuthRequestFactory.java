8eceb0f1cd654a66753d2cd5e67a887cf33c8ce0
Yangkai.Shen <237497819@qq.com>
2019-10-09 15:29:18 +0800

    :sparkles: `自定义扩展第三方平台`

--- src/main/java/com/xkcoding/justauth/AuthRequestFactory.java
+++ src/main/java/com/xkcoding/justauth/AuthRequestFactory.java
@@ -17,16 +17,25 @@
 
 package com.xkcoding.justauth;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.EnumUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.xkcoding.justauth.properties.ExtendProperties;
 import com.xkcoding.justauth.properties.JustAuthProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.zhyd.oauth.cache.AuthStateCache;
 import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.config.AuthDefaultSource;
 import me.zhyd.oauth.config.AuthSource;
 import me.zhyd.oauth.enums.AuthResponseStatus;
 import me.zhyd.oauth.exception.AuthException;
 import me.zhyd.oauth.request.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -37,6 +46,7 @@ import java.util.stream.Collectors;
  * @author yangkai.shen
  * @date Created in 2019-07-22 14:21
  */
+@Slf4j
 @RequiredArgsConstructor
 public class AuthRequestFactory {
     private final JustAuthProperties properties;
@@ -47,8 +57,17 @@ public class AuthRequestFactory {
      *
      * @return Oauth列表
      */
+    @SuppressWarnings("unchecked")
     public List<String> oauthList() {
-        return properties.getType().keySet().stream().map(Enum::name).collect(Collectors.toList());
+        // 默认列表
+        List<String> defaultList = properties.getType().keySet().stream().map(Enum::name).collect(Collectors.toList());
+
+        Class enumClass = properties.getExtend().getEnumClass();
+        List<String> names = EnumUtil.getNames(enumClass);
+        // 扩展列表
+        List<String> extendList = properties.getExtend().getConfig().keySet().stream().filter(x -> names.contains(x.toUpperCase())).map(String::toUpperCase).collect(Collectors.toList());
+        // 合并
+        return (List<String>) CollUtil.addAll(defaultList, extendList);
     }
 
     /**
@@ -57,9 +76,85 @@ public class AuthRequestFactory {
      * @param source {@link AuthSource}
      * @return {@link AuthRequest}
      */
-    public AuthRequest get(AuthSource source) {
-        AuthConfig config = properties.getType().get(source);
-        switch (source) {
+    public AuthRequest get(String source) {
+        if (StrUtil.isBlank(source)) {
+            throw new AuthException(AuthResponseStatus.NO_AUTH_SOURCE);
+        }
+
+        // 获取 JustAuth 中已存在的
+        AuthRequest authRequest = getDefaultRequest(source);
+
+        // 如果获取不到则尝试取自定义的
+        if (authRequest == null) {
+            authRequest = getExtendRequest(properties.getExtend().getEnumClass(), source);
+        }
+
+        if (authRequest == null) {
+            throw new AuthException(AuthResponseStatus.UNSUPPORTED);
+        }
+
+        return authRequest;
+    }
+
+    /**
+     * 获取自定义的 request
+     *
+     * @param clazz  枚举类 {@link AuthSource}
+     * @param source {@link AuthSource}
+     * @return {@link AuthRequest}
+     */
+    @SuppressWarnings("unchecked")
+    private AuthRequest getExtendRequest(Class clazz, String source) {
+        try {
+            EnumUtil.fromString(clazz, source.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // 无自定义匹配
+            return null;
+        }
+
+        Map<String, ExtendProperties.ExtendRequestConfig> extendConfig = properties.getExtend().getConfig();
+
+        // key 转大写
+        Map<String, ExtendProperties.ExtendRequestConfig> upperConfig = new HashMap<>(6);
+        extendConfig.forEach((k, v) -> upperConfig.put(k.toUpperCase(), v));
+
+        ExtendProperties.ExtendRequestConfig extendRequestConfig = upperConfig.get(source.toUpperCase());
+        if (extendRequestConfig != null) {
+            Class<? extends AuthRequest> requestClass = extendRequestConfig.getRequestClass();
+
+            if (requestClass != null) {
+                // 反射获取 Request 对象，所以必须实现 2 个参数的构造方法
+                return ReflectUtil.newInstance(requestClass, (AuthConfig) extendRequestConfig, authStateCache);
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * 获取默认的 Request
+     *
+     * @param source {@link AuthSource}
+     * @return {@link AuthRequest}
+     */
+    private AuthRequest getDefaultRequest(String source) {
+        AuthDefaultSource authDefaultSource;
+
+        try {
+            authDefaultSource = EnumUtil.fromString(AuthDefaultSource.class, source.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // 无自定义匹配
+            return null;
+        }
+
+        AuthConfig config = properties.getType().get(authDefaultSource);
+        // 找不到对应关系，直接返回空
+        if (config == null) {
+            return null;
+        }
+
+        switch (authDefaultSource) {
             case GITHUB:
                 return new AuthGithubRequest(config, authStateCache);
             case WEIBO:
@@ -116,8 +211,12 @@ public class AuthRequestFactory {
                 return new AuthGitlabRequest(config, authStateCache);
             case KUJIALE:
                 return new AuthKujialeRequest(config, authStateCache);
+            case ELEME:
+                return new AuthElemeRequest(config, authStateCache);
+            case MEITUAN:
+                return new AuthMeituanRequest(config, authStateCache);
             default:
-                throw new AuthException(AuthResponseStatus.UNSUPPORTED);
+                return null;
         }
     }
 }
